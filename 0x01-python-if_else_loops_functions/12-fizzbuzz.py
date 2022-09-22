ef36ea2c457b80edef650a7195523ae01751a75f
mwangisamkariuki <mwangisamkariuki@gmail.com>
2022-09-22 03:12:47 +0300

    a function that prints the numbers from 1 to 100 , with Fizz buzz at multiples of 3 and 5 respectifeully

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -10,8 +10,8 @@ def fizzbuzz():
     For all other numbers, print them as they are.
     """
     print(' '.join([
-        (i % 3 is 0) * 'Fizz' +
-        (i % 5 is 0) * 'Buzz' +
+        (i % 3 == 0) * 'Fizz' +
+        (i % 5 == 0) * 'Buzz' +
         (str(i) if i % 3 and i % 5 else "")
         for i in range(1, 101)
     ]), end=" ")
