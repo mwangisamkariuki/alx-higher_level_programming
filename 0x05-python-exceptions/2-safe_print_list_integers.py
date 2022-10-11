31971fdba74466b7bb2c5572951ce00f9b65cc5e
mwangisamkariuki <mwangisamkariuki@gmail.com>
2022-10-11 17:10:36 +0300

    .gitignore

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -4,3 +4,7 @@ def safe_print_list_integers(my_list=[], x=0):
     for i in range(x):
         print("{}".format(my_list[i]), end="")
         ret += 1
+    except (TypeError,ValueError,IndexError):
+        break
+    print("")
+    return(ret)
