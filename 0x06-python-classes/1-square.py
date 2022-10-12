335e4d83307c676862e4eb5e39f04be12913b402
mwangisamkariuki <mwangisamkariuki@gmail.com>
2022-10-12 17:16:38 +0300

    Write a class Square that defines a square by: (based on first file cretaed

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -7,5 +7,8 @@ class Square:
     # This is the starting point of a class
 
     def __init__(self, size):
-
+        """define an empty Square class
+        Args:
+            Square size is of type (int)
+        """
         self.__size = size
--- 0x06-python-classes/README.md
+++ 0x06-python-classes/README.md
@@ -1,3 +1,5 @@
+# Comments are requred
+
 # Task 0. My first square
 	Write an empty class Square that defines a square:
 	You are not allowed to import any module
