158aa9f9bc61bcb9f2749acd5d0d4555f7ca4caa
mwangisamkariuki <mwangisamkariuki@gmail.com>
2022-10-14 23:56:48 +0300

    Write a function that adds 2 integers

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
 """
-This is a simple module That takes 2 parameters, 
+This is a simple module That takes 2 parameters,
 adds and returns the results of the params of int type
 
 """
 
+
 def add_integer(a, b=98):
     """
-    Return the sum of two int and Raise:TypeError if either of a or b is not int or a float
+    Return the sum of two nums and TypeError if a or b is not int or a float
     """
     if ((not isinstance(a, int) and not isinstance(a, float))):
-        raise TypeError ("a must be an integer")
+        raise TypeError("a must be an integer")
     if((not isinstance(b, int) and not isinstance(b, float))):
-        raise TypeError ("b must be an integer")
+        raise TypeError("b must be an integer")
     return (int(a) + int(b))
