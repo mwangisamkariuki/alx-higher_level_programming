080e2aba7ba754f7a1053c1ed65b4f13fcd6e6fc
mwangisamkariuki <mwangisamkariuki@gmail.com>
2022-10-17 23:03:30 +0300

    Detect instance deletion

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -58,10 +58,9 @@ class Rectangle:
             if i != self.__height - 1:
                 rect.append("\n")
         return ("".join(rect))
-    
+
     def __repr__(self):
         """Return the string representation of the Rectangle."""
         rect = "Rectangle(" + str(self.__width)
         rect += ", " + str(self.__height) + ")"
         return (rect)
-
