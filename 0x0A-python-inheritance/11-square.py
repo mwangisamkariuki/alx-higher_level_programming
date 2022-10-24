0aa38dd5bba40f5c1e595c17fe5ea724e5988ff3
mwangisamkariuki <mwangisamkariuki@gmail.com>
2022-10-25 00:45:19 +0300

    Square as a subclass of a derived class

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Defines a Rectangle and its subclass (square)."""
+"""Defines a Rectangle and its (square)."""
 Rectangle = __import__('9-rectangle').Rectangle
 
 
@@ -12,5 +12,9 @@ class Square(Rectangle):
             size (int): The size of the new square.
         """
         self.integer_validator("size", size)
-        super().__init__(size, size)
         self.__size = size
+        super().__init__(self.__size, self.__size)
+
+    def __str__(self):
+        """print method"""
+        return "[{}] {}/{}".format(__class__.__name__, self.__size, self.__size)
