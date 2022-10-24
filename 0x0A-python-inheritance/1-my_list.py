70718ac161f0d18a9311423382efdbd05182abab
mwangisamkariuki <mwangisamkariuki@gmail.com>
2022-10-24 19:05:29 +0300

    my list ammended

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-"""A module that contains a list"""
+"""defines a module MyList inherited from list"""
 
 
 class MyList(list):
-    """pints a sorted list"""
+    """prints a sorted list"""
 
     def print_sorted(self):
         """prints a list sorted in ascending order"""
