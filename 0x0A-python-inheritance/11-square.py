639fcb4c5a362db3c1d184c3e6f5808abe8742bd
mwangisamkariuki <mwangisamkariuki@gmail.com>
2022-10-25 00:53:14 +0300

    Square as a subclass of a derived class

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -17,4 +17,5 @@ class Square(Rectangle):
 
     def __str__(self):
         """print method"""
-        return "[{}] {}/{}".format(__class__.__name__, self.__size, self.__size)
+        return "[{}] {}/{}" \
+            .format(__class__.__name__, self.__size, self.__size)
