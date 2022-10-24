b6c1cf76122e522586501b1348a112042ce8660e
mwangisamkariuki <mwangisamkariuki@gmail.com>
2022-10-25 00:13:48 +0300

    a class BaseGeometry (based on 6-base_geometry.py

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -1,22 +1,23 @@
 #!/usr/bin/python3
-"""Defines Module impliments Geometry"""
+"""Defines Module impliments Geometry
+"""
 
 
 class BaseGeometry:
-    """a class that represents a base Geometry"""
+    """represents a base Geometry"""
 
     def area(self):
         """Area Not Implimented"""
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        """function to validate parameters as an Int
-        TypeError if Value is not an Int
+        """
+        function to validate parameters as an int
         """
         self.name = name
         self.value = value
 
-        if type(value) != int:
+        if type(value) is not int:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,7 +1,5 @@
-
 #!/usr/bin/python3
 """Defines Module impliments Geometry"""
-
 
 
 class BaseGeometry:
