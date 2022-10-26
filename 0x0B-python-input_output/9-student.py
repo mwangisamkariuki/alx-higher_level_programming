5ecc2700ec7bfc6dcaf45abc8ab5b39c0ae42309
mwangisamkariuki <mwangisamkariuki@gmail.com>
2022-10-26 12:28:50 +0300

    a class Student that defines a student by:

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,19 +1,15 @@
 #!/usr/bin/python3
-#!/usr/bin/python3
 """impliments student to json"""
 
 
 class Student:
     """Represent a student."""
 
-
     def __init__(self, first_name, last_name, age):
         """initialize an new student"""
-
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
-
 
     def to_json(self):
         """retrienves a dictionary repr of a student instance"""
