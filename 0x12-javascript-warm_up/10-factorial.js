8af6ad440d2c9b1ccbb057c54467b15b5fd7e519
mwangisamkariuki <mwangisamkariuki@gmail.com>
2022-12-19 17:05:33 +0300

    Factorial

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -2,7 +2,7 @@
 // Print the factorial of an argument recursively
 function factorial (x) {
     if (isNaN(n)) {
-        return 1;
+      return 1;
     }
     if (x > 1) {
       return x * factorial(x - 1);
@@ -10,6 +10,6 @@ function factorial (x) {
     if (x < 0) {
       return undefined;
     }
-  }
+}
   const n = parseInt(process.argv[2]);
   console.log(factorial(n));
