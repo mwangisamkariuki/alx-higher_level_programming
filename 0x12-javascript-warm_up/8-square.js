9ca775e4044a7340ffdf83ccf8738b9e47245d74
mwangisamkariuki <mwangisamkariuki@gmail.com>
2022-12-19 16:18:04 +0300

    Square

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -2,13 +2,13 @@
 // a script that prints a square
 // argument is the size of the square
 // if arg cannot be converted to int, print "Missing size"
-const size = Math.floor(Number(process.argv[2]));
-if (isNaN(size)) {
+const x = Math.floor(Number(process.argv[2]));
+if (isNaN(x)) {
   console.log('Missing size');
 } else {
-  for (let r = 0; r < size; r++) {
+  for (let r = 0; r < x; r++) {
     let row = '';
-    for (let c = 0; c < size; c++) row += 'X';
+    for (let c = 0; c < x; c++) row += 'X';
     console.log(row);
   }
 }
