222015dedd3a234a46f19069778a670e1fd20291
mwangisamkariuki <mwangisamkariuki@gmail.com>
2022-12-19 15:26:28 +0300

    a script that prints x times “C is fun”

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+// Check if it is a number
+// Print 'C is fun" times the argument passed
+// Else print 'Missing number of occurences' if no argument
+const num = Math.floor(Number(process.argv[2]));
+if (isNaN(num)){
+  console.log('Missing number of occurences');
+}
+else {
+  for(let i = 0; i < num; i++) {
+  console.log('C is fun')        
+  }
+}
