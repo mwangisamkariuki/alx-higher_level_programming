436966894d8055023311ff4bdd52ceef35878726
mwangisamkariuki <mwangisamkariuki@gmail.com>
2022-12-19 16:48:07 +0300

    Factorial

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 //Factorial Function recursive
 function factorial (n) {
-    return n === 0 || isNaN(n) ? 1 : n * factorial(n - 1);
-  }
+  return n === 0 || isNaN(n) ? 1 : n * factorial(n - 1);
+}
 
-  console.log(factorial(Number(process.argv[2])));
+console.log(factorial(Number(process.argv[2])));
