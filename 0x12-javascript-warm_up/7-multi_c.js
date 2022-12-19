d0a2cd736d65c9ee46e37029eaecf7f58e2a9009
mwangisamkariuki <mwangisamkariuki@gmail.com>
2022-12-19 15:32:11 +0300

    a script that prints x times “C is fun”

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -5,9 +5,8 @@
 const x = Math.floor(Number(process.argv[2]));
 if (isNaN(x)){
   console.log('Missing number of occurrences');
-}
-else {
-  for(let i = 0; i < x; i++) {
-  console.log('C is fun');       
+} else {
+  for (let i = 0; i < x; i++) {
+    console.log('C is fun');       
   }
 }
