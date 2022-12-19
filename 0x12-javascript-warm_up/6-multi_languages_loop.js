38cd9c1966c9919f56986303da66f5fe32dafe22
mwangisamkariuki <mwangisamkariuki@gmail.com>
2022-12-19 14:51:02 +0300

    loop using for

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const langs = ['C is fun','Python is cool','C is fun'];
+const langs = ['C is fun','Python is cool','JavaScript is amazing'];
 for (let i=0; i < langs.length; i++){
     console.log(langs[i]);
 }
