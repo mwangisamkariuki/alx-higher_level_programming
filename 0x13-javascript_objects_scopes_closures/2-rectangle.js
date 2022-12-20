c962f04cf9db9de589c3f7e32ca3489f35056c77
mwangisamkariuki <mwangisamkariuki@gmail.com>
2022-12-20 14:47:51 +0300

    Rectangle #2

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -2,11 +2,11 @@
 class Rectangle {
     constructor (h, w) {
       if (( h > 0) && (w > 0)) {
-        this.height = h;
         this.width = w;
+        this.height = h;
       }
     }
-  }
-  
+}
+ 
 module.exports = Rectangle;
   
