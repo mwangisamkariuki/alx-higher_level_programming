ff2c739c21a7236becb46bef21f6faea0d06eb2c
mwangisamkariuki <mwangisamkariuki@gmail.com>
2022-12-20 16:54:11 +0300

    Square #1 inheritance and Instantiation

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -7,7 +7,7 @@ module.exports = class Square extends require('./5-square.js') {
       for (let i = 0; i < this.height; i++) {
         let s = '';
         for (let j = 0; j < this.width; j++) {
-          s += 'c';
+          s += c;
         }
         console.log(s);
       }
