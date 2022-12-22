dcbdf6653432e15494d5162b7af4b3434d164407
mwangisamkariuki <mwangisamkariuki@gmail.com>
2022-12-22 21:39:43 +0300

    SQL safe Injection

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+This script is safe from sql injection
+a script that takes in an argument and displays
+all values in the states table of hbtn_0e_0_usa
+where name matches the argument.
+"""
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(user=sys.argv[1], port=3306, host="localhost",
+                         passwd=sys.argv[2], db=sys.argv[3])
+    querry = db.cursor()
+    querry.execute("SELECT * FROM states WHERE name LIKE %s ORDER BY \
+    id ASC", (argv[4]))
+    states = querry.fetchall()
+    for state in states:
+        if state[1] == sys.argv[4]:
+            print(state)
+    querry.close()
+    db.close()
--- 0x0F-python-object_relational_mapping/README.md
+++ 0x0F-python-object_relational_mapping/README.md
@@ -1,7 +1,7 @@
 # Task 1: a script that lists all states from the database hbtn_0e_0_usa
 # Task 2: a script that lists all states with a name starting with N (upper N),from the database hbtn_0e_0_usa:
 # Task 3: Filter states by user input
-# Task 3:
+# Task 4: SQl Injection
 # Task 3:
 # Task 3:
 # Task 3:
