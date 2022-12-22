4b4f103326e39b1eac26e784c8a10c1763ade652
mwangisamkariuki <mwangisamkariuki@gmail.com>
2022-12-22 23:15:19 +0300

    Import update

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -8,7 +8,7 @@ Results must display the states.id
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from model_state import State, Base
+from model_state import Base, State
 
 if __name__ == "__main__":
     engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
