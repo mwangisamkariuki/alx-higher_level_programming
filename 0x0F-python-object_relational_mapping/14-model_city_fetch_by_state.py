3432043f6368f08364a6815f9b36c6cb0d08eccd
mwangisamkariuki <mwangisamkariuki@gmail.com>
2022-12-22 23:34:01 +0300

    A model and a querry

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -19,6 +19,6 @@ if __name__ == "__main__":
     st_cty = session.query(State, City).filter(State.id == City.state_id).all()
 
     for city, state in session.query(City, State) \
-                            .filter(City.state_id == State.id) \
-                            .order_by(City.id):
+                              .filter(City.state_id == State.id) \
+                              .order_by(City.id):
         print("{}: ({}) {}".format(state.name, city.id, city.name))
