e1af9c7c65c9b0ae7e4bd1e795e3ee0c4484285f
mwangisamkariuki <mwangisamkariuki@gmail.com>
2022-12-22 21:54:43 +0300

    SqL iNJECTION

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -5,18 +5,17 @@ a script that takes in an argument and displays
 all values in the states table of hbtn_0e_0_usa
 where name matches the argument.
 """
-import sys
 import MySQLdb
+from sys import argv
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(user=sys.argv[1], port=3306, host="localhost",
-                         passwd=sys.argv[2], db=argv[3])
-
-    cursor = db.cursor()
-    cursor.execute("SELECT * FROM states WHERE name LIKE %s ORDER BY id ASC",
-                    (argv[4],))
-    states = cursor.fetchall()
-    for state in states:
-        print(state)
-    cursor.close()
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3], charset="utf8")
+    querry = db.cursor()
+    querry.execute("SELECT * FROM states WHERE name LIKE %s ORDER BY id ASC",
+                   (argv[4],))
+    rows = querry.fetchall()
+    for row in rows:
+        print(row)
+    querry.close()
     db.close()
