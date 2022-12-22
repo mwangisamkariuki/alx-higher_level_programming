dbbc717ce1c6662b0de9445f0743e550626c949d
mwangisamkariuki <mwangisamkariuki@gmail.com>
2022-12-22 21:53:13 +0300

    SQl Injection

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -11,11 +11,12 @@ import MySQLdb
 if __name__ == "__main__":
     db = MySQLdb.connect(user=sys.argv[1], port=3306, host="localhost",
                          passwd=sys.argv[2], db=argv[3])
-    querry = db.cursor()
-    querry.execute("SELECT * FROM states WHERE name LIKE %s ORDER BY \
-    id ASC", (argv[4],))
-    states = querry.fetchall()
+
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states WHERE name LIKE %s ORDER BY id ASC",
+                    (argv[4],))
+    states = cursor.fetchall()
     for state in states:
         print(state)
-    querry.close()
+    cursor.close()
     db.close()
