9b5b1f8c8428e6577c3d46d342b83cb57c3b5f44
mwangisamkariuki <mwangisamkariuki@gmail.com>
2022-12-22 22:12:43 +0300

    a script that takes in the name of a state as an argument and lists all cities

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """
-List all cities from a database
+a script that lists all cities from the database
+hbtn_0e_4_usa
+Results must be sorted in ascending order by cities.id
 """
 import sys
 import MySQLdb
@@ -10,7 +12,8 @@ if __name__ == '__main__':
 
     querry = db.cursor()
     querry.execute("SELECT cities.id, cities.name, states.name \
-    FROM cities JOIN states ON cities.state_id = states.id;")
+    FROM cities JOIN states ON cities.state_id = states.id \
+    ORDER BY cities.id ASC;")
     states = querry.fetchall()
 
     for state in states:
--- 0x0F-python-object_relational_mapping/README.md
+++ 0x0F-python-object_relational_mapping/README.md
@@ -2,8 +2,8 @@
 # Task 2: a script that lists all states with a name starting with N (upper N),from the database hbtn_0e_0_usa:
 # Task 3: Filter states by user input
 # Task 4: SQl Injection
-# Task 3:
-# Task 3:
+# Task 5: Cities by states
+# Task 6: All cities by state argument
 # Task 3:
 # Task 3:
 # Task 3:
