385af6f32b659b10a874f2ab3303f567366cbe8c
mwangisamkariuki <mwangisamkariuki@gmail.com>
2023-06-16 15:59:40 +0300

    scripts that sends a POST request to a given URL

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Send POST request to a URL and display the body of the response
-curl -sL -d 'test@gmail.com' -d 'subject=I will always be here for PLD' -- "$1"
+curl -s -X POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
